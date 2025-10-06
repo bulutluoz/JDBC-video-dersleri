a50cd44c5a6eb7fab9797ea5e3223fdb32f0d02e
bulutluoz <abbulutluoz@gmail.com>
2025-10-06 21:14:06 +0200

    5.ders sonu

--- src/test/java/ders05_dinamikQueryOlusturma/C02_isimTesti.java
+++ src/test/java/ders05_dinamikQueryOlusturma/C02_isimTesti.java
@@ -1,15 +1,18 @@
-package ders04_frameworkOlmadanDatabaseTesti;
+package ders05_dinamikQueryOlusturma;
+
+import manageQueries.LoantechQueries;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class C01_UsernameListesiOlusturma {
+public class C02_isimTesti {
 
     public static void main(String[] args) throws SQLException {
-        // Loantech uygulamasini kullanan kullanicilarin
-        // username listesini yazdirin.
+        // Loantech uygulamasındaki users tablosunda
+        // name bilgisi “hasan” olan kaydın var olduğunu doğrulayın.
 
         // 1.Adim: Veri tabanına bağlanma ve bağlantıyı kaydetme
-
         String url = "jdbc:mysql://195.35.59.18/u201212290_qaloantec";
         String username = "u201212290_qaloanuser";
         String password = "HPo?+7r$";
@@ -17,28 +20,28 @@ public class C01_UsernameListesiOlusturma {
         Connection connection = DriverManager.getConnection(url,username,password);
 
         // 2.Adim : SQL sorgusunu hazırlama
-        String selectUsername = "SELECT * FROM users";
+        String query = LoantechQueries.usersTablosundakiNameSorgusu;
 
         // 3.Adim: SQL sorgusunu çalıştırma ve sonuç setini kaydetme
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-        ResultSet usernameResultSet = statement.executeQuery(selectUsername);
+        ResultSet resultset = statement.executeQuery(query);
 
         // 4.Adim: resultSet objesini iterator gibi kullanarak istenen işlemi yapma
-        System.out.println(usernameResultSet);
-        // resultSet direkt yazdirilamaz
-        // Iterator gibi kullanarak, satirlari elden gecirmeli
-        // ve satirlarda istedigimiz islemi yapmaliyiz
+        List<String> nameList = new ArrayList<>();
 
-        while (usernameResultSet.next()){
+        while (resultset.next()){
 
-            System.out.println(usernameResultSet.getString("username"));
+            nameList.add(resultset.getString("firstname"));
         }
-        usernameResultSet.deleteRow();
+
+        if (nameList.contains("hasan")){
+            System.out.println("Test PASSED");
+        }else System.out.println("Test FAILED");
 
         // 5.Adim: Database bağlantısını kapatma
-        usernameResultSet.close();
+        resultset.close();
         statement.close();
         connection.close();
-    }
 
+    }
 }
--- src/test/java/ders05_dinamikQueryOlusturma/C03_kayitSayisiTesti.java
+++ src/test/java/ders05_dinamikQueryOlusturma/C03_kayitSayisiTesti.java
@@ -1,44 +1,50 @@
-package ders04_frameworkOlmadanDatabaseTesti;
+package ders05_dinamikQueryOlusturma;
+
+import manageQueries.LoantechQueries;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class C01_UsernameListesiOlusturma {
+public class C03_kayitSayisiTesti {
 
     public static void main(String[] args) throws SQLException {
-        // Loantech uygulamasini kullanan kullanicilarin
-        // username listesini yazdirin.
+        // Loantech uygulamasındaki subscribers tablosunda
+        // toplam 21 kaydın var olduğunu doğrulayın.
 
         // 1.Adim: Veri tabanına bağlanma ve bağlantıyı kaydetme
-
         String url = "jdbc:mysql://195.35.59.18/u201212290_qaloantec";
         String username = "u201212290_qaloanuser";
         String password = "HPo?+7r$";
 
         Connection connection = DriverManager.getConnection(url,username,password);
 
+
         // 2.Adim : SQL sorgusunu hazırlama
-        String selectUsername = "SELECT * FROM users";
+        String query = LoantechQueries.subscribersEmailSorgusu;
 
         // 3.Adim: SQL sorgusunu çalıştırma ve sonuç setini kaydetme
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-        ResultSet usernameResultSet = statement.executeQuery(selectUsername);
+        ResultSet resultset = statement.executeQuery(query);
 
         // 4.Adim: resultSet objesini iterator gibi kullanarak istenen işlemi yapma
-        System.out.println(usernameResultSet);
-        // resultSet direkt yazdirilamaz
-        // Iterator gibi kullanarak, satirlari elden gecirmeli
-        // ve satirlarda istedigimiz islemi yapmaliyiz
+        List<String> emailList = new ArrayList<>();
 
-        while (usernameResultSet.next()){
+        while (resultset.next()){
 
-            System.out.println(usernameResultSet.getString("username"));
+            emailList.add(resultset.getString("email"));
         }
-        usernameResultSet.deleteRow();
+
+        if (emailList.size()==21){
+            System.out.println("Test PASSED");
+        } else System.out.println("Test FAILED");
+
 
         // 5.Adim: Database bağlantısını kapatma
-        usernameResultSet.close();
+        resultset.close();
         statement.close();
         connection.close();
-    }
 
+
+    }
 }
